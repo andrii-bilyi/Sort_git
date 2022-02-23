1de5f37f73112ccd01b9fd2ed2c78f96e936864f
Андрей <@DESKTOP-U3QJHLN>
2022-02-23 14:34:49 +0200

    commit Task #1

--- Core/.cpp/Foo.cpp
+++ Core/.cpp/Foo.cpp
@@ -1,1 +1,52 @@
 #include "../../SystemLib.h"
+
+void InitArray(int arr[], int size) {
+	srand(time(NULL));
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 20;
+	}
+
+}
+void ShowArray(int arr[], int size) {
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << '\t';
+	}
+	cout << endl;
+}
+void SortArray(int arr[], int size, bool X) {
+	int temp = 0;
+	if (X == 1)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = size - 1; j > i; j--)
+			{
+				if (arr[j - 1] < arr[j])
+				{
+					temp = arr[j - 1];
+					arr[j - 1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+	else if (X == 0)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = size - 1; j > i; j--)
+			{
+				if (arr[j - 1] > arr[j])
+				{
+					temp = arr[j - 1];
+					arr[j - 1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+
+}
--- Core/.h/Foo.h
+++ Core/.h/Foo.h
@@ -1,3 +1,7 @@
 #include "../../SystemLib.h"
 
+//============= Задание 1 =============== 
+void InitArray(int arr[], int size);
+void ShowArray(int arr[], int size);
+void SortArray(int arr[], int size, bool X);
 
